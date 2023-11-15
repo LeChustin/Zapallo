0d655e6388dc7c0564ebdc499561be38e7983b08
LeChustin <jostinaldaz4@gmail.com>
2023-11-15 10:57:53 -0500

    pantalla que suma 2 numeros

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.chstn.zapallo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.chstn.zapallo"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/chstn/zapallo/MainActivity.java
+++ app/src/main/java/com/chstn/zapallo/MainActivity.java
@@ -1,14 +1,54 @@
 package com.chstn.zapallo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextNumber1;
+    private EditText editTextNumber2;
+    private Button btnSumar;
+    private TextView textViewResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextNumber1 = findViewById(R.id.editTextNumber1);
+        editTextNumber2 = findViewById(R.id.editTextNumber2);
+        btnSumar = findViewById(R.id.btnSumar);
+        textViewResultado = findViewById(R.id.textViewResultado);
+
+        btnSumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sumarNumeros();
+            }
+        });
+    }
+
+    private void sumarNumeros() {
+        // Obtener los números ingresados
+        String strNumero1 = editTextNumber1.getText().toString();
+        String strNumero2 = editTextNumber2.getText().toString();
+
+        // Verificar que se hayan ingresado números
+        if (!strNumero1.isEmpty() && !strNumero2.isEmpty()) {
+
+            double numero1 = Double.parseDouble(strNumero1);
+            double numero2 = Double.parseDouble(strNumero2);
+            double resultado = numero1 + numero2;
+
+
+            textViewResultado.setText("Resultado: " + resultado);
+        } else {
+
+            textViewResultado.setText("Por favor, ingrese ambos números.");
+        }
     }
 }
