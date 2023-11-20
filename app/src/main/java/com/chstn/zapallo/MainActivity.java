8317729eabda9ed2f252b884968dc21e681b70d3
LeChustin <jostinaldaz4@gmail.com>
2023-11-20 01:55:10 -0500

    creacion de la aplicacion

--- app/src/main/java/com/chstn/zapallo/MainActivity.java
+++ app/src/main/java/com/chstn/zapallo/MainActivity.java
@@ -1,54 +1,40 @@
 package com.chstn.zapallo;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editTextNumber1;
-    private EditText editTextNumber2;
-    private Button btnSumar;
-    private TextView textViewResultado;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        editTextNumber1 = findViewById(R.id.editTextNumber1);
-        editTextNumber2 = findViewById(R.id.editTextNumber2);
-        btnSumar = findViewById(R.id.btnSumar);
-        textViewResultado = findViewById(R.id.textViewResultado);
-
-        btnSumar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sumarNumeros();
-            }
-        });
     }
 
-    private void sumarNumeros() {
-        // Obtener los números ingresados
-        String strNumero1 = editTextNumber1.getText().toString();
-        String strNumero2 = editTextNumber2.getText().toString();
+    public void openSumaActivity(View view) {
+        Intent intent = new Intent(this, OperacionActivity.class);
+        intent.putExtra("operacion", "Suma");
+        startActivity(intent);
+    }
 
-        // Verificar que se hayan ingresado números
-        if (!strNumero1.isEmpty() && !strNumero2.isEmpty()) {
+    public void openRestaActivity(View view) {
+        Intent intent = new Intent(this, OperacionActivity.class);
+        intent.putExtra("operacion", "Resta");
+        startActivity(intent);
+    }
 
-            double numero1 = Double.parseDouble(strNumero1);
-            double numero2 = Double.parseDouble(strNumero2);
-            double resultado = numero1 + numero2;
+    public void openMultiplicacionActivity(View view) {
+        Intent intent = new Intent(this, OperacionActivity.class);
+        intent.putExtra("operacion", "Multiplicación");
+        startActivity(intent);
+    }
 
-
-            textViewResultado.setText("Resultado: " + resultado);
-        } else {
-
-            textViewResultado.setText("Por favor, ingrese ambos números.");
-        }
+    public void openDivisionActivity(View view) {
+        Intent intent = new Intent(this, OperacionActivity.class);
+        intent.putExtra("operacion", "División");
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/chstn/zapallo/OperacionActivity.java
@@ -0,0 +1,60 @@
+package com.chstn.zapallo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OperacionActivity extends AppCompatActivity {
+
+    private EditText editTextNumero1;
+    private EditText editTextNumero2;
+    private TextView textViewResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operacion);
+
+        editTextNumero1 = findViewById(R.id.editTextNumero1);
+        editTextNumero2 = findViewById(R.id.editTextNumero2);
+        textViewResultado = findViewById(R.id.textViewResultado);
+    }
+
+    public void calcularOperacion(View view) {
+        double numero1 = Double.parseDouble(editTextNumero1.getText().toString());
+        double numero2 = Double.parseDouble(editTextNumero2.getText().toString());
+        String operacion = getIntent().getStringExtra("operacion");
+
+        double resultado = 0;
+
+        switch (operacion) {
+            case "Suma":
+                resultado = numero1 + numero2;
+                break;
+            case "Resta":
+                resultado = numero1 - numero2;
+                break;
+            case "Multiplicación":
+                resultado = numero1 * numero2;
+                break;
+            case "División":
+                if (numero2 != 0
+                ) {
+                    resultado = numero1 / numero2;
+                } else {
+                    textViewResultado.setText("No es posible dividir por cero");
+                    return;
+                }
+                break;
+            default:
+                textViewResultado.setText("Operación no reconocida");
+                return;
+        }
+
+        // Muestra el resultado en el TextView
+        textViewResultado.setText("Resultado: " + resultado);
+    }
+}
